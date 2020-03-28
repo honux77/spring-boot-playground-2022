a6498123bb105dea246d482feddd8de3ae29af9e
Hoyoung Jung <honux77@gmail.com>
2020-03-29 01:42:52 +0900

    Setup OK
    로그 설정, H2 설정
    간단 부트 테스트 코드 작성

--- src/test/java/net/honux/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/net/honux/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,32 @@
 package net.honux.springbootdemo;
 
-import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.*;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext ctx;
+	private Logger logger = LoggerFactory.getLogger(SpringBootDemoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		assertThat(ctx).isNotNull();
+		logger.debug("ApplicationContext is not null");
 	}
+
+	@Test
+	void LoggerNotNull() {
+		assertThat(logger).isNotNull();
+		logger.debug("Logger OK");
+	}
+
 
 }
