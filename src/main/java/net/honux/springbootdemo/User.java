69fb6bac8d014cbf0382697e4f2156069804eece
Hoyoung Jung <honux77@gmail.com>
2020-04-01 00:06:54 +0900

    Revert "수업시간에 구현한 one-to-one code"
    This reverts commit 071d86adbeb5d0d13ba16dad081dd3d5974bd925.

--- src/main/java/net/honux/springbootdemo/User.java
+++ src/main/java/net/honux/springbootdemo/User.java
@@ -3,42 +3,21 @@ package net.honux.springbootdemo;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class User {
 
     @Id
     private Long id;
     private String email;
-    private Github github;
+    private LocalDateTime createdDate;
 
-    public User(String email, Github github) {
-        this.email = email;
-        this.github = github;
-    }
-
-    public void setGithub(Github github) {
-        this.github = github;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Github getGithub() {
-        return github;
-    }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", email='" + email + '\'' +
-                ", github=" + github +
+                ", createdDate=" + createdDate +
                 '}';
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,2 @@
-DROP TABLE IF EXISTS github;
 DROP TABLE IF EXISTS user;
-
-CREATE TABLE IF NOT EXISTS user (id int auto_increment primary key, email varchar(64) unique);
-
-CREATE TABLE github(
-    id int auto_increment primary key,
-    user int references user(id),
-    github_id varchar(64)
-)
+CREATE TABLE IF NOT EXISTS user (id identity, email varchar(64) unique, created_date datetime);
